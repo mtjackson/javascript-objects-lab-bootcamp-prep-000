770da922ac6fffa771ea1a8b97a86e7a193bd580
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-09 01:40:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object[key] = value;
   return object;
 }
 
